dee9cfb5ba2feec2170adf23426ef19b6f47ca83
DajLx <durandaj027@gmail.com>
2025-05-01 08:43:08 -0400

    a part of Front done.

---
+++ Frontend/src/components/mainView.jsx
@@ -0,0 +1,67 @@
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import FakeData from "../utils/fakeData.json";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
+
+
+console.log("entre en el mainview");
+
+const MainView = () => {
+  const user = useSelector((state) => state.user);
+  console.log(user);
+  const navi = useNavigate();
+  const [description, setDescription] = useState(null);
+  return (
+    <Container>
+      
+      <Row>
+        {FakeData.ofertas.map((ofertas) => {
+          return (
+            <Col key={ofertas.id} xs={6} style={{ border: "black solid  1px" }}>
+              <Card>
+                <Card.Img
+                  variant="top"
+                  src={ofertas.imgSrc}
+                  style={{ height: "25rem" }}
+                />
+                <Card.Body>
+                  <Card.Title>{ofertas.producto}</Card.Title>
+                  <Card.Text>
+                    Ahorre hasta un {ofertas.descuento} de descuento, disponible
+                    hasta {ofertas.disponible_hasta}.
+                  </Card.Text>
+                  <Button
+                    variant="primary"
+                    style={{ marginRight: "1rem" }}
+                    onClick={() => {
+                      setDescription(
+                        description === ofertas.id ? null : ofertas.id
+                      );
+                    }}
+                  >
+                    {description === ofertas.id
+                      ? "Ocultar Descripcion"
+                      : "Descripcion"}
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      navi("/login");
+                    }}
+                  >
+                    Comprar {ofertas.precio_oferta}$
+                  </Button>
+
+                  <Card.Text style={{ marginTop: "1rem" }}>
+                    {description === ofertas.id ? ofertas.descripcion : null}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+};
+export default MainView;
